641e613916c94364700f7953e4a75b7639ae4c9a
Your Name <you@example.com>
2017-09-14 00:09:12 +0800

    This version can add a pin on the map by long press the screen, but if I hold tapping, more and more pins will appear on the map.

--- MyVirtualTourist/MapViewController.swift
+++ MyVirtualTourist/MapViewController.swift
@@ -11,11 +11,45 @@ import MapKit
 
 class mapViewController: UIViewController, MKMapViewDelegate {
     
-    // Mark: properties
+    // Mark: Properties
+    
+    var coordinate = CLLocationCoordinate2D()
     
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Add longPress gesture
+        let longPress = UILongPressGestureRecognizer(target:self,
+                                                     action:#selector(addAPin(_:)))
+        self.view.addGestureRecognizer(longPress)
+    }
+    
+    // Add a pin on the map
+    func addAPin(_ sender: UILongPressGestureRecognizer){
+        
+        // Create an annotation model
+        let annotation = MKPointAnnotation()
+        
+        // Add annotation's properties
+        annotation.coordinate = coordinate
+        annotation.title = "title"
+        annotation.subtitle = "subtitle"
+        
+        // Add annotation model on the map
+        mapView.addAnnotation(annotation)
+    }
+    
+    // Get the location
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        // Get the touch point location
+        let point = touches.first?.location(in: mapView)
+        
+        // Turn the location to coordinate
+        coordinate = mapView.convert(point!, toCoordinateFrom: mapView)
     }
 }
+
+

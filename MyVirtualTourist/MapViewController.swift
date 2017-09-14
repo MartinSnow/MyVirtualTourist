0eb76732bac5047249027e03ef5af5383d22b5d9
Your Name <you@example.com>
2017-09-14 19:51:53 +0800

    Fix the problem about adding more and more pins by adding an line "if gestureRecognizer.state != .began {return}"

--- MyVirtualTourist/MapViewController.swift
+++ MyVirtualTourist/MapViewController.swift
@@ -27,7 +27,18 @@ class mapViewController: UIViewController, MKMapViewDelegate {
     }
     
     // Add a pin on the map
-    func addAPin(_ sender: UILongPressGestureRecognizer){
+    func addAPin(_ gestureRecognizer: UIGestureRecognizer){
+        
+        // Check start state
+        if gestureRecognizer.state != .began {
+            return
+        }
+        
+        // Get the touch point location
+        let touchPoint = gestureRecognizer.location(in: mapView)
+        
+        // Turn the location to coordinate
+        coordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
         
         // Create an annotation model
         let annotation = MKPointAnnotation()
@@ -39,16 +50,6 @@ class mapViewController: UIViewController, MKMapViewDelegate {
         
         // Add annotation model on the map
         mapView.addAnnotation(annotation)
-    }
-    
-    // Get the location
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        // Get the touch point location
-        let point = touches.first?.location(in: mapView)
-        
-        // Turn the location to coordinate
-        coordinate = mapView.convert(point!, toCoordinateFrom: mapView)
     }
 }
 
